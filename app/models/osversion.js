be65bb4778b43fcedded71034cfff91e6179c276
Emanuel Schütze <emanuel@intevation.de>
2017-01-12 14:50:00 +0100

    Added osversion.name.

--- app/models/osversion.js
+++ app/models/osversion.js
@@ -1,6 +1,7 @@
 import DS from 'ember-data';
 
 export default DS.Model.extend({
-  image: DS.attr('string'),
-  default: DS.attr('boolean')
+    image: DS.attr('string'),
+    name: DS.attr('string'),
+    default: DS.attr('boolean')
 });
