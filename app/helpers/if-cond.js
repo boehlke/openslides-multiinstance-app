e4ee68be40bbd3fd899e229deb62a01e22db5439
André Böhlke <ab@bcoding.de>
2017-04-12 23:52:03 +0200

    Cleanup and use eslint to support async/await

--- app/helpers/if-cond.js
+++ app/helpers/if-cond.js
@@ -1,37 +1,35 @@
 import Ember from 'ember';
 
-export default Ember.Helper.helper(function(args) {
-    var v1 = args[0];
-    var operator = args[1];
-    var v2 = args[2];
-    switch (operator) {
-        case '==':
-            /* jshint ignore:start */
-            return (v1 == v2) ? true : false;
-            /* jshint ignore:end */
-        case '===':
-            return (v1 === v2) ? true : false;
-        case '===':
-            return (v1 === v2) ? true : false;
-        case '!==':
-            return (v1 !== v2) ? true : false;
-        case '!=':
-            /* jshint ignore:start */
-            return (v1 != v2) ? true : false;
-            /* jshint ignore:end */
-        case '<':
-            return (v1 < v2) ? true : false;
-        case '<=':
-            return (v1 <= v2) ? true : false;
-        case '>':
-            return (v1 > v2) ? true : false;
-        case '>=':
-            return (v1 >= v2) ? true : false;
-        case '&&':
-            return (v1 && v2) ? true : false;
-        case '||':
-            return (v1 || v2) ? true : false;
-        default:
-            return false;
-    }
+export default Ember.Helper.helper(function (args) {
+  const v1 = args[0];
+  const operator = args[1];
+  const v2 = args[2];
+  switch (operator) {
+    case '==':
+      /* jshint ignore:start */
+      return (v1 == v2) ? true : false;
+    /* jshint ignore:end */
+    case '===':
+      return (v1 === v2) ? true : false;
+    case '!==':
+      return (v1 !== v2) ? true : false;
+    case '!=':
+      /* jshint ignore:start */
+      return (v1 != v2) ? true : false;
+    /* jshint ignore:end */
+    case '<':
+      return (v1 < v2) ? true : false;
+    case '<=':
+      return (v1 <= v2) ? true : false;
+    case '>':
+      return (v1 > v2) ? true : false;
+    case '>=':
+      return (v1 >= v2) ? true : false;
+    case '&&':
+      return (v1 && v2) ? true : false;
+    case '||':
+      return (v1 || v2) ? true : false;
+    default:
+      return false;
+  }
 });
