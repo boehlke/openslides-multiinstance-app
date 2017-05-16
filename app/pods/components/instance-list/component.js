444331403c99c617e03ed8ab1f14328ce1212cd2
André Böhlke <ab@bcoding.de>
2017-05-16 22:22:23 +0200

    Add sorting and version column to instance table (#19)

--- app/pods/components/instance-list/component.js
+++ app/pods/components/instance-list/component.js
@@ -1,12 +1,45 @@
-import Ember from 'ember';
+import Ember from "ember";
+
+const sortFunctions = {
+  name: function (a, b) {
+    return a.get('event_name').toLowerCase().localeCompare(b.get('event_name').toLowerCase());
+  },
+  version: function (a, b) {
+    return a.get('osversion.id') - b.get('osversion.id');
+  },
+  status: function (a, b) {
+    let state = a.get('state');
+    let state2 = b.get('state');
+    return state ? state.localeCompare(state2) : -1;
+  }
+};
 
 export default Ember.Component.extend({
+  sortAscending: true,
   savedInstances: Ember.computed.filter('model', function (instance) {
     return !!instance.get('slug');
   }),
-  sortedInstances: Ember.computed.sort('savedInstances', function (a, b) {
-    return a.get('event_name').toLowerCase() > b.get('event_name').toLowerCase();
+  sortedProperties: ['name'],
+  sortedInstances: Ember.computed('savedInstances', 'sortedProperties.@each', 'sortAscending', function () {
+    let sortedProperty = this.get('sortedProperties')[0];
+    let fnCompare = this.get('sortAscending') ?
+      function(a, b) { return sortFunctions[sortedProperty](a, b); } :
+      function (a, b) { return sortFunctions[sortedProperty](b, a);};
+    return this.get('savedInstances').sort(fnCompare);
   }),
+  sortedOnName: Ember.computed('sortedProperties.[]', function () {
+    return this.get('sortedProperties')[0] === 'name';
+  }),
+  sortedOnVersion: Ember.computed('sortedProperties.[]', function () {
+    return this.get('sortedProperties')[0] === 'version';
+  }),
+  sortedOnStatus: Ember.computed('sortedProperties.[]', function () {
+    return this.get('sortedProperties')[0] === 'status';
+  }),
+  glyphiconDirection: Ember.computed('sortAscending', function () {
+    return this.get('sortAscending') ? "glyphicon-chevron-down" : "glyphicon-chevron-up";
+  }),
+
   store: Ember.inject.service(),
   initRefresh: function () {
     const refresh = function () {
@@ -17,7 +50,7 @@ export default Ember.Component.extend({
     setInterval(3000, refresh);
   }.on('init'),
   actions: {
-    upgradeInstance: function(instance) {
+    upgradeInstance: function (instance) {
       this.sendAction('upgradeInstance', instance);
     },
     removeInstance: function (instance) {
@@ -32,6 +65,14 @@ export default Ember.Component.extend({
     },
     update: function () {
       this.sendAction('update');
+    },
+    sortBy: function (property) {
+      if(this.get('sortedProperties')[0] === property) {
+        this.toggleProperty('sortAscending');
+      } else {
+        this.set('sortAscending', true);
+        this.set('sortedProperties', [property]);
+      }
     }
   }
 });
